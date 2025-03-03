57ca8cb5677e254f6ad1d449f53075c07a037f7d
Ivan Aragonés Muniesa <26822043+ivarmu@users.noreply.github.com>
2025-03-03 16:45:12 +0100

    Make 2.adapt_venv.sh parameterized

--- 2.adapt_venv.sh
+++ 2.adapt_venv.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: ${0} <venv_dir>
+fi
+
+INPUT_VENV="${1}"
+
 # Make the virtual environment to be relocatable
-virtualenv --relocatable awscliv2
+virtualenv --relocatable "${INPUT_VENV}"
 
 # Re-create the links that are not updated by the 'virtualenv --relocatable' command
-find awscliv2/ -type l -ls | grep $PWD | while read a b c d e f g h i j dest l orig; do
+find "${INPUT_VENV}/" -type l -ls | grep $PWD | while read a b c d e f g h i j dest l orig; do
   rm -f ${dest}
   ln -srf ${orig//${PWD}/.} ${dest}
 done
 
 # Update the virtualenv activation script (only take care of the bash version)
-sed -i 's,^VIRTUAL_ENV.*$,VIRTUAL_ENV="${BASH_SOURCE%%/bin/activate}",' awscliv2/bin/activate
+sed -i 's,^VIRTUAL_ENV.*$,VIRTUAL_ENV="${BASH_SOURCE%%/bin/activate}",' "${INPUT_VENV}/bin/activate"
 
 # Compress the virtual environment so it can be installed into a container in next steps
-tar cvf - awscliv2 | gzip -v9 > awscliv2.venv.tar.gz
+tar cvf - "${INPUT_VENV}" | gzip -v9 > "${INPUT_VENV}.venv.tar.gz"
