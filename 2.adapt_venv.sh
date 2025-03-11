78d25faea0c8c13c080c875b87814fe3dd64c00f
root <root@rhel7server.iam.lab>
2025-03-11 07:39:18 -0400

    updates

--- 2.adapt_venv.sh
+++ 2.adapt_venv.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
 # Make the virtual environment to be relocatable
-virtualenv --relocatable ${1}
+# virtualenv --relocatable ${1}
 
-# Re-create the links that are not updated by the 'virtualenv --relocatable' command
+# Remove all the 'pyc' files in the venv
+find ${1} -name "*.pyc" -exec rm -f {} \;
+
+# Remove the links so they become the final files
+#find ${1}/ -type l -ls | grep "/usr/" | while read a b c d e f g h i j dest l orig; do
 find ${1}/ -type l -ls | grep $PWD | while read a b c d e f g h i j dest l orig; do
+
   rm -f ${dest}
-  ln -srf ${orig//${PWD}/.} ${dest}
+  cp -a ${orig} ${dest}
 done
 
 # Update the virtualenv activation script (only take care of the bash version)
 sed -i 's,^VIRTUAL_ENV.*$,VIRTUAL_ENV="${BASH_SOURCE%%/bin/activate}",' ${1}/bin/activate
+# grep -lRE '^#\!/.*/bin/.*' ${1} | while read file; do
+#   sed -i 's,^#\!/.*/bin/.*,,' ${file}
+# done
 
 # Compress the virtual environment so it can be installed into a container in next steps
 tar cf - ${1} | gzip -9 > ${1}.venv.tar.gz
