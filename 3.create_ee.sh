21347c2bb23b33853a1d2f57b0b7331a6aa2d173
ivarmu <26822043+ivarmu@users.noreply.github.com>
2025-03-14 08:44:05 +0100

    the tag must be always lowercase

--- 3.create_ee.sh
+++ 3.create_ee.sh
@@ -8,12 +8,13 @@ fi
 AAH=${1}
 USR=${2}
 IMG=${3}
+IMG_L=${IMG@L}
 
 read -s -p "Type the ${USR}'s password to push the image to ${AAH}: " PWD
 
 # Build the container
-ansible-builder build --build-arg venv_name="${IMG%:*}" --tag ${AAH}/${IMG%:*}_legacy:${IMG#*:} || exit 2
+ansible-builder build --build-arg venv_name="${IMG%:*}" --tag ${AAH}/${IMG_L%:*}_legacy:${IMG_L#*:} || exit 2
 
 # Publish the container
 podman login "${AAH}" -u "${USR}" -p "${PWD}" --tls-verify=false || exit 3
-podman push "${AAH}/${IMG%:*}_legacy:${IMG#*:}" --tls-verify=false
+podman push "${AAH}/${IMG_L%:*}_legacy:${IMG_L#*:}" --tls-verify=false
